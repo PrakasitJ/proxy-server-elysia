907a0d7f7a8ee2deaa268adf48428db039d8e3b3
Prakasit J. <big0231@gmail.com>
2024-09-25 23:03:21 +0700

    Fix: Status Problem

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,5 +4,6 @@ const middleware = new Elysia().onRequest(async ({ set }) => {
   set.headers["access-control-allow-methods"] =
     "GET, POST, PUT, DELETE, OPTIONS";
   set.headers["access-control-allow-headers"] = "Content-Type, Authorization";
+  set.status = 200;
 });
 export default middleware;
